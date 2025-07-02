3b591d8b2cf51ec46e3a69dbed942d134d5777d9
Tommaso Franzini <franzint@tcd.ie>
2025-07-02 16:06:22 +0200

    test

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -5,7 +5,8 @@
 #include <iostream>
 
 int main(){
-    float num_1, num_2, result;
+    float num_1=0;
+    float num_2, result;
 
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
